d62a5deb6b2467ada492c9cee2bc3b508129c3f8
lecamarena <lcamarenas@paux.cdmx.gob.mx>
2019-10-03 10:46:52 -0500

    sdsd

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ and open the template in the editor.
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="#" style="color: white">Iniciar sesión <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="#" style="color: white">Iniciar sfesión <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#" style="color: white">Productos</a>
